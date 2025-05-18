f939392cfdb12b95a2a05c78e99ff7e3fa3437ba
youngnigth <youngdesert01@gmail.com>
2025-05-17 22:09:54 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -117,7 +117,7 @@ const modal = document.getElementById("newsletter-modal");
             setTimeout(() => {
               modal.classList.add("hidden");
               document.body.style.overflow = "auto";
-            }, 1500);
+            }, 2000);
           } else {
             alert("Oops! Something went wrong.");
             resetButton();
